5e5c2437976ed7bd621cd631d358860faf3b71bf
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-08 16:44:39 +0200

    Prima parte modificata - inizio Parte bonus

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-//Funzione generale che si attiva al click di ogni singola colonna
-$('.col').click(function(){
+// Funzione generale del gioco
+function coloraQuadrati(){
 
-  //Controllo se la colonna cliccata ha la classe red e in caso positivo il background diventa rosso
-  if ($(this).hasClass('red')) {
-    $(this).css('background','red');
-  }
-  // In caso contrario aggiungo una classe green fatta in css con cui coloro il background di verde
-  else{
-    $(this).addClass('green')
-  }
-});
+  // Creo due variabili per salvare quanti quadratini rossi o verdi scopriamo:
+  var i=0;
+  var j=0;
+
+  // Si va a costruire una funzione anonima che al click mi permetta di eseguire il controllo
+  $('.col').click(function(){
+
+    // Se il col cliccato ha la classe red mi applicherà una modifica inline al css
+    if ($(this).hasClass('red')) {
+      $(this).css('background','red');
+      i++;
+    }
+
+    // In caso contrario aggiungo una classe green fatta in css con cui coloro il background di verde
+    else{
+      $(this).addClass('green');
+      j++;
+    }
+    
+  });
+};
+
+coloraQuadrati();
